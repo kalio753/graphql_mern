3c0ec4556a3a56036426cd4e1afa1ba102726733
kalio753 <duongchikien2001@gmail.com>
2022-10-17 09:04:25 +0700

    Project and Client query

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const path = require("path")
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
+const { graphqlHTTP } = require("express-graphql")
+const schema = require("./schema/schema")
+const connectDB = require("./config/db")
+const port = process.env.PORT || 5000
+
+const app = express()
+
+// Connect to Database
+connectDB()
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        graphiql: process.env.NODE_ENV === "development",
+    })
+)
+
+app.listen(port, console.log(`Server is running on port ${port}`))
